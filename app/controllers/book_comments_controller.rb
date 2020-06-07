e705d488734a9e0f1601fe2c78be9f90639eebc9
Ryo-15 <tomorrows.way15@gmail.com>
2020-06-07 20:38:12 +0900

    いいね、コメント非同期通信実装

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,22 +1,31 @@
 class BookCommentsController < ApplicationController
+
   def create
-    book = Book.find(params[:book_id])
-    comment = BookComment.new(book_comment_params)
-    comment.user_id = current_user.id
-    comment.book_id = book.id
-    comment.save
+    @book = Book.find(params[:book_id])
+    @book_new = Book.new
+    @book_comment = current_user.book_comments.new(book_comment_params)
+    # @book_comment = BookComment.new(book_comment_params)
+    # @book_comment.user_id = current_user.id
+    @book_comment.book_id = @book.id
+    # @book_comment.book_id = params[:book_id].to_i
+    @book_comment.save
+    @book_comments = Book.find(params[:book_id]).book_comments
     # 直線の画面に戻るリダイレクト（＋できなかった場合のパス）
-    redirect_back(fallback_location: books_path)
+    # redirect_back(fallback_location: books_path)
+    # 非同期通信にするためコメントアウト
   end
 
   def destroy
-    book_comment = BookComment.find(params[:book_id])
-    book_comment.destroy
+    @book_comment = BookComment.find(params[:book_id])
+    @book_comment.destroy
+    @book_comments = Book.find(@book_comment.book_id).book_comments
     # 直線の画面に戻るリダイレクト（＋できなかった場合のパス）
-    redirect_back(fallback_location: books_path)
+    # redirect_back(fallback_location: books_path)
+    # 非同期通信にするためコメントアウト
   end
 
   private
+
   def book_comment_params
     params.require(:book_comment).permit(:comment)
   end
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,18 +1,22 @@
 class FavoritesController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.new(book_id: book.id)
+    # favorite viewに飛ばすので、@をつける
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.new(book_id: @book.id)
     favorite.save
     # 直線の画面に戻るリダイレクト（＋できなかった場合のパス）
-    # redirect_back(fallback_location: books_path) 非同期通信にするためコメントアウト
+    # redirect_back(fallback_location: books_path)
+    # 非同期通信にするためコメントアウト
   end
 
   def destroy
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.find_by(book_id: book.id)
+    # favorite viewに飛ばすので、@をつける
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.find_by(book_id: @book.id)
     favorite.destroy
     # 直線の画面に戻るリダイレクト（＋できなかった場合のパス）
-    # redirect_back(fallback_location: books_path) 非同期通信にするためコメントアウト
+    # redirect_back(fallback_location: books_path)
+    # 非同期通信にするためコメントアウト
   end
 
 end
